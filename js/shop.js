5a6c81fd3f3892c1b7a3df4f77e3c97b8a6ffe30
dq-heard <dq.heard@yahoo.com>
2023-07-09 01:09:10 -0500

    Carousel Fix

--- js/shop.js
+++ js/shop.js
@@ -43,19 +43,33 @@ const products = [
   },
 ];
 
-const carousel = document.querySelector(".carousel");
 const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+// Create clones of the entire products array
+const productsClone1 = [...products];
+const productsClone2 = [...products];
 
-products.forEach(item => {
+let isDragging = false;
+let isAutoPlay = true;
+let startX, startScrollLeft, timeoutId;
+
+// Add clones to the beginning and end of the carousel
+productsClone1.forEach(item => createCard(item, -1));
+products.forEach(item => createCard(item, 0));
+productsClone2.forEach(item => createCard(item, 1));
+
+// Function to create a card element and append it to the carousel
+function createCard(item, order) {
   const card = document.createElement("li");
   card.className = "card";
+  card.style.order = order;
 
   const icons = document.createElement("div");
   icons.className = "icons";
   icons.innerHTML = `<a href="#" class="fas fa-heart"></a>
-                      <a href="#" class="fas fa-share"></a>
-                      <a href="#" class="fas fa-eye"></a>
-                    `;
+                     <a href="#" class="fas fa-share"></a>
+                     <a href="#" class="fas fa-eye"></a>`;
 
   const img = document.createElement("img");
   img.className = "product-img";
@@ -75,10 +89,10 @@ products.forEach(item => {
   stars.className = "stars";
   stars.setAttribute("aria-hidden", "true");
   stars.innerHTML = `<i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="fas fa-star"></i>
-                      <i class="far fa-star"></i>`;
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="far fa-star"></i>`;
 
   const btn = document.createElement("a");
   btn.className = "btn";
@@ -91,45 +105,20 @@ products.forEach(item => {
   card.appendChild(stars);
   card.appendChild(btn);
 
-
   carousel.appendChild(card);
-});
+}
 
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 // Get the number of cards that can fit in the carousel at once
-let firstCardWidth = carousel.querySelector(".card").offsetWidth;
+let cardWidth = carousel.querySelector(".card").offsetWidth;
 
-window.addEventListener('resize', () => {
-  firstCardWidth = carousel.querySelector(".card").offsetWidth;
+window.addEventListener("resize", () => {
+  cardWidth = carousel.querySelector(".card").offsetWidth;
 });
-
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth) + 1;
-const carouselChildren = [...carousel.children];
-
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildren.slice(-cardPerView).reverse().forEach(card => {
-  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildren.slice(0, cardPerView).forEach(card => {
-  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
-
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
 
 // Add event listeners for the arrow buttons to scroll the carousel left and right
-const arrowBtns = document.querySelectorAll(".wrapper i");
-
-let isButtonClicked = false;
-
-arrowBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
-    isButtonClicked = true;
-    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+arrowBtns.forEach(button => {
+  button.addEventListener("click", e => {
+    carousel.scrollLeft += (e.target.id === "left" ? -1 : 1) * cardWidth;
   });
 });
 
@@ -137,37 +126,36 @@ const dragStart = (e) => {
   isDragging = true;
   carousel.classList.add("dragging");
   // Records the initial cursor and scroll position of the carousel
-  startX = e.pageX;
-  startScrollLeft = carousel.scrollLeft;
+  startX = e.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
-  if(!isDragging) return; // if isDragging is false return from here
+  if (!isDragging) return; // if isDragging is false return from here
+  e.preventDefault();
   // Updates the scroll position of the carousel based on the cursor movement
-  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+  const x = e.pageX - carousel.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  carousel.scrollLeft = scrollLeft - walk;
 }
 const dragStop = () => {
   isDragging = false;
   carousel.classList.remove("dragging");
 }
+
 // Add event listener for scroll event on the carousel element
 const infiniteScroll = carousel.addEventListener('scroll', () => {
-  if (isButtonClicked) {
-    isButtonClicked = false;
-    return;
-  }
-
-  // Check if user has scrolled to the end of the carousel
-  if (carousel.scrollLeft + carousel.offsetWidth + 10 >= carousel.scrollWidth) {
-    // Scroll back to beginning of carousel
-    carousel.classList.add("no-transition");
-    carousel.scrollLeft = carousel.offsetWidth;
-    carousel.classList.remove("no-transition");
-  }
   // Check if user has scrolled to the beginning of the carousel
-  else if (carousel.scrollLeft <= 0) {
+  if (carousel.scrollLeft === 0) {
     // Scroll back to end of carousel
     carousel.classList.add("no-transition");
-    carousel.scrollLeft = carousel.scrollWidth - (firstCardWidth * cardPerView) - carousel.offsetWidth;
+    carousel.scrollLeft += products.length * cardWidth;
+    carousel.classList.remove("no-transition");
+  } 
+  // Check if user has scrolled to the end of the carousel
+  else if (Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    // Scroll back to beginning of carousel
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft -= products.length * cardWidth;
     carousel.classList.remove("no-transition");
   }
 
@@ -176,11 +164,10 @@ const infiniteScroll = carousel.addEventListener('scroll', () => {
   if(!wrapper.matches(":hover")) autoPlay();
 });
 
-   
 const autoPlay = () => {
   if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
   // Autoplay the carousel after every 2500 ms
-  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+  timeoutId = setTimeout(() => carousel.scrollLeft += cardWidth, 2500);
 }
 autoPlay();
 carousel.addEventListener("mousedown", dragStart);
